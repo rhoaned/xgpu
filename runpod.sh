7915a26bb7c319873766a4a4429357a94c1df419
rhoaned <95939850+rhoaned@users.noreply.github.com>
2023-12-01 15:33:36 +1100

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -16,7 +16,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xB8d6681fa406f9D065bc89643d4e6611b1886cf3/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
